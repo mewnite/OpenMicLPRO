913b5469e7fa1b4c11d5fad7d430f7b36d1edf8a
mewnite <83909337+mewnite@users.noreply.github.com>
2024-08-31 23:50:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstati
 
 // Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyA3NbOrszfuDgQT_I0fda-rugxdeKurM_4",
+  authDomain: "registerlpro.firebaseapp.com",
+  projectId: "registerlpro",
+  storageBucket: "registerlpro.appspot.com",
+  messagingSenderId: "139406970618",
+  appId: "1:139406970618:web:239599a6c56bcff6ee8343"
 };
 
 // Inicializar Firebase
